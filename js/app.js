b9fba3bd4f02626e4889a912c1b57c09c3f2c90b
reboot13-git <rautkrutik013@gmail.com>
2022-06-04 11:05:46 +0530

    updated final

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ let servicesInfo=[
         "linkText": "Get your own Website now →", 
         "id": "web"
 
+
     },
     {
         "imgUrl": "https://img.icons8.com/nolan/64/mobile-package-tracking.png",
@@ -17,35 +18,35 @@ let servicesInfo=[
 
     },
     {
-        "imgUrl": "https://img.icons8.com/nolan/64/edge-constraint.png",
-        "heading": "UI/UX Designing",
-        "description" : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis reprehenderit recusandae rerum ab placeat! At cum atque quos velit ut praesentium esse ipsum sunt quo?",
-        "linkText": "Get an awesome UI Design →",
-        "id": "uiux"
-
-    },
-    {
         "imgUrl": "https://img.icons8.com/nolan/64/adobe-illustrator.png",
-        "heading": "Graphic Designing",
-        "description" : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis reprehenderit recusandae rerum ab placeat! At cum atque quos velit ut praesentium esse ipsum sunt quo?",
-        "linkText": "Get your own Website now →",
-        "id": "graphd"
+        "heading": "Designing",
+        "description" : "We help you deal with talented designers whether it be graphics or UI/UX designs. Make your project stand out and power up your brand, with our effective and quality services.",
+        "linkText": "Get an awesome design →",
+        "id": "design"
 
     },
     {
         "imgUrl": "https://img.icons8.com/nolan/64/video.png",
-        "heading": "Content Writing",
-        "description" : "Content that moves people, Yes please. We make content that makes you stand out and presence known.Discuss your next project for the best engaging writings.",
-        "linkText": "Get an App for your business →",
+        "heading": "Video Editing",
+        "description" : "We transform your raw footage into professional video. Excelling at making your videos from simple to stunning, We turn your given content into impactful visuals.",
+        "linkText": "Get your video edited! →",
         "id": "vedit"
 
     },
     {
-        "imgUrl": "https://img.icons8.com/nolan/64/edge-constraint.png",
+        "imgUrl": "https://img.icons8.com/nolan/64/pen.png",
+        "heading": "Content Writing",
+        "description" : "Content that moves people, Yes please. We make content that makes you stand out and presence known.Discuss your next project for the best engaging writings.",
+        "linkText": "Get Content Writer →",
+        "id": "cntwrt"
+
+    },
+    {
+        "imgUrl": "https://img.icons8.com/nolan/64/commercial.png",
         "heading": "Digital Marketing",
-        "description" : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis reprehenderit recusandae rerum ab placeat! At cum atque quos velit ut praesentium esse ipsum sunt quo?",
-        "linkText": "Get an awesome UI Design →",
-        "id": "conwrt"
+        "description" : "Looking someone to help you advertise and follow-up your business? We produce strategies and give results which have max reach to your business and help you maintain your social media.",
+        "linkText": "Market your business online →",
+        "id": "dimark"
     }
 ]
 
@@ -59,9 +60,7 @@ scards.innerHTML += `<div class="col "  data-aos="fade-up-right" >
 <img src=${item.imgUrl} alt=${item.heading} />
 <h4> ${item.heading} </h4>
 <p> ${item.description} </p>
-<a href='#temp'>
-  <h6>${item.linkText}</h6>
-</a>
+<button onclick="scrollToDiv(this.value)" value="contactus" class="nav-link p-0  "><h6>${item.linkText}</h6></button>
 </div>
 </div>`
     })
